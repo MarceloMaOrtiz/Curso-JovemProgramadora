b586a470fe8b576d3d67f555085d2033a618989a
Marcelo <marcelo.ortiz@edu.es.senac.br>
2025-07-24 21:36:46 -0300

    Escreva inicial

---
+++ console/Git-Inicial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Inicial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // escreva("Marcelo! Boa noite!")
+            Console.WriteLine("Marcelo! Boa noite!");
+        }
+    }
+}
