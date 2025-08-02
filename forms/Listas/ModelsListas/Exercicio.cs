3cb2b32b9cccdb964cc091de6f8a33083b6cd335
mmortiz <marcelo.ortiz@edu.es.senac.br>
2025-08-02 20:51:51 -0300

    Pequenas correções

--- forms/Listas/ModelsListas/Exercicio.cs
+++ forms/Listas/ModelsListas/Exercicio.cs
@@ -5,14 +5,14 @@ namespace ModelsListas
     public class Exercicio
     {
 
-        public int Id { get; private set };
-        public string Nome { get; private set };
-        public string Titulo { get; private set };
-        public string Descricao { get; private set };
-        public string Resposta { get; private set };
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public string Resposta { get; private set; }
 
-        public DateTime DataCriacao { get : private set };
-        public DateTime DataAtualizacao { get : private set };
+        public DateTime DataCriacao { get; private set; }
+        public DateTime DataAtualizacao { get; private set; }
 
         public Exercicio(string nome, string titulo, string descricao, string resposta)
         {
