c486a96bd2e94807f6c2f5ed996b19d6a366cbb9
Marcelo <marcelo.ortiz@edu.es.senac.br>
2025-08-07 21:54:59 -0300

    Criação da Classe Animal

---
+++ console/ExemploPOO/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO
+{
+    public class Animal
+    {
+        //public string Nome { get; set; }
+        //public int Idade { get; set; }
+
+        public string nome;
+        public int idade;
+        public string especie;
+        public string classe;
+        public bool vivo;
+
+        public Animal()
+        { }
+
+        public Animal(string nome, int idade, string especie, string classe, bool vivo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.especie = especie;
+            this.classe = classe;
+            this.vivo = vivo;
+        }
+    }
+}
---
+++ console/ExemploPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal01 = new Animal();
+            animal01.nome = "Laboom";
+            animal01.idade = 100;
+            animal01.especie = "Baleia";
+            animal01.classe = "Mamífero";
+            animal01.vivo = true;
+
+            Animal animal02 = new Animal("Simba", 23, "Leão", "Mamífero", true);
+        }
+    }
+}
