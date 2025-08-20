46c58bf009abb2d77451b0c900f7528a86fe207e
mmortiz <marcelo.ortiz@edu.es.senac.br>
2025-08-20 12:48:56 -0300

    Alteração para o arquivo desenvolvido pelas alunas para o gerencimento de Alunos

--- console/GerenciamentoAlunosRAM/Program.cs
+++ console/GerenciamentoAlunosRAM/Program.cs
@@ -4,215 +4,175 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GerenciamentoAlunosRAM
+namespace gerenciamento_de_turma
 {
     internal class Program
     {
+        static int cadastro = 0;
 
-        private const int MAX_ALUNO = 100;
-
-        private static int CUR_ALUNO = 0;
-        public static void menu()
-        {
-            Console.Write("\n\n##### MENU - Aluno #####\n");
-            Console.Write("\t1- Cadastrar\n");
-            Console.Write("\t2- Remover\n");
-            Console.Write("\t3- Buscar por CPF\n");
-            Console.Write("\t4- Listar todos\n");
-            Console.Write("\t5- Listar Aprovados\n");
-            Console.Write("\t6- Listar Reprovados\n");
-            Console.Write("\t7- Encerrar\n");
-            Console.Write("Opção: ");
+        public static bool ValidaCadastro(){
+            if (cadastro == 0){
+                Console.WriteLine("\nNão existem alunos cadastrados!");
+                return false;
+            }
+            return true;    
         }
 
-        public static bool validarCPF(string[][] matriz, string cpf)
+        public static void CadastroAluno(string[,] matriz)
         {
-            for(int i = 0; i < CUR_ALUNO; i++){
-                if(matriz[i][2] == cpf){
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public static void cadastrar(string[][] matriz)
-        {
-            //Console.ReadLine();
-            string nome, dt_nascimento, cpf, media;
-            Console.Write("\n##### Cadastrar Aluno #####\n");
-            Console.Write("\tCPF: ");
-            cpf = Console.ReadLine();
-            if(validarCPF(matriz, cpf)){
-                Console.Write("\tNome: ");
-                nome = Console.ReadLine();
-                Console.Write("\tData de Nascimento: ");
-                dt_nascimento = Console.ReadLine();
-                Console.Write("\tMédia: ");
-                media = Console.ReadLine();
-
-                matriz[CUR_ALUNO][0] = nome;
-                matriz[CUR_ALUNO][1] = dt_nascimento;
-                matriz[CUR_ALUNO][2] = cpf;
-                matriz[CUR_ALUNO][3] = media;
-                matriz[CUR_ALUNO][4] = "true";
-
-                CUR_ALUNO++;
-
-                //Console.Write("Aluno " + nome + " cadastrado com sucesso.\n");
-                Console.Write($"Aluno {nome} cadastrado com sucesso.\n");
-            }
-            else{
-                Console.Write("\nCPF já existente.\n");
-            }
-        }
-
-        public static int buscaCPF(string[][] matriz, string cpf)
-        {
-            for(int i = 0; i < CUR_ALUNO; i++){
-                if(matriz[i][2] == cpf){
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-        public static void remover(string[][] matriz)
-        {
+            bool fazerCadastro = true;
             string cpf;
-            int indice;
 
-            Console.Write("\n##### Remover Aluno #####\n");
-            Console.Write("\tCPF: ");
-
+            Console.WriteLine("Preencha as informações a seguir:\nCPF: ");
             cpf = Console.ReadLine();
-            indice = buscaCPF(matriz, cpf);
 
-            if (indice != -1) {
-                matriz[indice][4] = "false";
-                //Console.Write("Aluno " + matriz[indice][0] + " removido.");
-                Console.Write($"Aluno {matriz[indice][0]} removido.\n");
+            for (int i = 0; i < cadastro; i++){
+                if(matriz[i,2] == cpf){
+                    Console.WriteLine("\nO CPF informado já existe!\n");
+                    fazerCadastro = false;
+                    if(!Convert.ToBoolean(matriz[i,4])){
+                        int opcao;
+                        Console.WriteLine("Aluno desativado, deseja reativar?\n1 - Sim\n2 - Não\nOpção: ");
+                        opcao = Convert.ToInt32(Console.ReadLine());
+                        switch (opcao) {
+                            case 1:
+                                matriz[i, 4] = "true";
+                                Console.WriteLine($"Aluno {matriz[i, 0]} reativado com sucesso!\n");
+                            break;
+                            case 2:
+                                Console.WriteLine($"Aluno {matriz[i, 0]} segue desativado!\n");
+                            break;
+                            default:
+                                Console.WriteLine("Opção inválida, retornando para o menu inicial.");
+                            break;
+                        }
+                        i = cadastro;
+                    }
+                }
             }
-            else {
-                //Console.Write("Aluno com CPF " + cpf + " não encontrado.");
-                Console.Write($"Aluno com CPF {cpf} não encontrado.");
+
+            if(fazerCadastro){
+                cpf = matriz[cadastro, 2];
+
+                Console.WriteLine("Nome:");
+                matriz[cadastro,0] = Console.ReadLine();
+
+                Console.WriteLine("Data de nascimento:");
+                matriz[cadastro, 1] = Console.ReadLine();
+
+                Console.WriteLine("Média:");
+                matriz[cadastro, 3] = Console.ReadLine();
+
+                matriz[cadastro, 4] = "true";
+
+                Console.WriteLine($"Aluno {matriz[cadastro,0]} cadastrado com sucesso!\n");
+
+                cadastro++;
+            }
+
+        }
+        
+        public static void ListarAtivos(string[,] matriz)
+        {
+            if(ValidaCadastro()){
+                for(int i = 0; i < cadastro; i++){
+                    if (Convert.ToBoolean(matriz[i, 4])){
+                        Console.WriteLine($"\nNome: {matriz[i,0]} \nData de nascimento: {matriz[i,1]}");
+                        Console.WriteLine($"CPF: {matriz[i,2]} \nMédia: {matriz[i,3]}");
+                    }
+                }
             }
         }
 
-        public static void listar(string[][] matriz, int indice)
-        {
-            Console.Write("\n\n\tNome: " + matriz[indice][0]);
-            Console.Write("\n\tData de Nascimento: " + matriz[indice][1]);
-            Console.Write("\n\tCPF: " + matriz[indice][2]);
-            Console.WriteLine("\n\tMédia: " + matriz[indice][3]);
-        }
-
-        public static void buscarAluno(string[][] matriz)
-        {
+        public static void RemoverAluno(string[,] matriz){
             string cpf;
-            int indice;
-
-            Console.Write("\n##### Buscar Aluno #####\n");
-            Console.Write("\tCPF: ");
-    
-            cpf = Console.ReadLine();
-
-            indice = buscaCPF(matriz, cpf);
-
-            if (indice != -1) {
-                listar(matriz, indice);
-            }
-            else {
-                //Console.Write("Aluno com CPF " + cpf + " não encontrado.");
-                Console.WriteLine($"Aluno com CPF {cpf} não encontrado.");
+                if(ValidaCadastro()){
+                Console.WriteLine("\nInsira o CPF do aluno que você deseja remover: ");
+                cpf = Console.ReadLine();
+                for(int i = 0; i < cadastro; i++){
+                    if(matriz[i,2] == cpf){
+                        matriz[i, 4] = "false";
+                        i = cadastro;
+                    }
+                }
+                Console.WriteLine("\nAluno removido com sucesso!");
             }
         }
 
-        public static void listarTodos(string[][] matriz)
-        {
-            Console.Write("\n##### Alunos #####\n");
+        public static void BuscarAluno(string[,] matriz){
+            string cpf;
+            if (ValidaCadastro()){
+                Console.WriteLine("\nInsira o CPF do aluno que você deseja encontrar: ");
+                cpf = Console.ReadLine();
     
-            for(int i = 0; i < CUR_ALUNO; i++){
-                if(bool.Parse(matriz[i][4])){
-                    listar(matriz, i);
+                for (int i = 0; i < cadastro; i++){
+                    if (Convert.ToBoolean(matriz[i, 4]) == false){
+                        Console.WriteLine("Aluno está desativado");
+                    }
+                    else if(matriz[i,2] == cpf){
+                        Console.WriteLine($"\nNome: {matriz[i, 0]} \nData de nascimento: {matriz[i, 1]}");
+                        Console.WriteLine($"CPF: {matriz[i, 2]} \nMédia: {matriz[i, 3]}");
+                        i = cadastro;
+                    }  
                 }
             }
         }
 
-        public static void listarAprovados(string[][] matriz)
-        {
-            Console.Write("\n##### Alunos Aprovados #####\n");
-    
-            for(int i = 0; i < CUR_ALUNO; i++){
-                if(bool.Parse(matriz[i][4]) && Convert.ToInt32(matriz[i][3]) >= 7){
-                    listar(matriz, i);
+        public static void Media(string[,] matriz, int opcao){
+            if(ValidaCadastro()){
+                if(opcao == 5){
+                    Console.WriteLine("\nAlunos aprovados: ");
+                    for(int i = 0; i < cadastro; i++){
+                        if (Convert.ToInt32(matriz[i,3]) >= 7 && Convert.ToBoolean(matriz[i, 4])){
+                            Console.WriteLine($"\n {matriz[i,0]}");
+                        }
+                    }
+                }
+                else{
+                    Console.WriteLine("\nAlunos reprovados: ");
+                    for (int i = 0; i < cadastro; i++){
+                        if (Convert.ToInt32(matriz[i, 3]) < 7 && Convert.ToBoolean(matriz[i, 4])){
+                            Console.WriteLine($"\n {matriz[i, 0]}");
+                        }
+                    }
                 }
             }
         }
+        static void Main(){
+            string[,] matriz = new string[100, 5];
+            int menu;
 
-        public static void listarReprovados(string[][] matriz)
-        {
-            Console.Write("\n##### Alunos Reprovados #####\n");
-    
-            for(int i = 0; i < CUR_ALUNO; i++){
-                if(bool.Parse(matriz[i][4]) && Convert.ToInt32(matriz[i][3]) < 7){
-                    listar(matriz, i);
-                }
-            }
-        }
+            do{
+                Console.WriteLine("\n\n Menu Principal:\n1 - Cadastrar aluno\n2 - Remover Aluno\n3 - Buscar Aluno\n4 - Listar todos os Alunos\n5 - Listar Alunos Aprovados\n6 - Listar Alunos Reprovados\n7 - Sair\nOpção: ");
+                menu = Convert.ToInt32(Console.ReadLine());
 
-        static void Main(string[] args)
-        {
-            string[][] alunos = new string[100][];
-
-            for(int i = 0; i < 5; i++)
-            {
-                alunos[i] = new string[5];
-            }
-
-            char opcao;
-
-            do {
-                menu();
-
-                opcao = (char)Console.Read();
-                Console.ReadLine();
-
-                switch (opcao)
-                {
-                    case '1':
-                        cadastrar(alunos);
-                        break;
-                    case '2':
-                        remover(alunos);
-
-                        break;
-                    case '3':
-                        buscarAluno(alunos);
-
-                        break;
-                    case '4':
-                        listarTodos(alunos);
-
-                        break;
-                    case '5':
-                        listarAprovados(alunos);
-
-                        break;
-                    case '6':
-                        listarReprovados(alunos);
-                        break;
-                    case '7':
-                        Console.Write("\n\tFIM !!\n");
-                        break;
+                switch (menu){
+                    case 1:
+                        CadastroAluno(matriz);
+                    break;
+                    case 2:
+                        RemoverAluno(matriz);
+                    break;
+                    case 3:
+                        BuscarAluno(matriz);
+                    break;
+                    case 4:
+                        ListarAtivos(matriz);
+                    break;
+                    case 5:
+                        Media(matriz, 5);
+                    break;
+                    case 6:
+                        Media(matriz, 6);
+                    break;
+                    case 7:
+                        Console.WriteLine("\nPrograma encerrado!");
+                    break;
                     default:
-                        Console.Write("Opção Inválida.\n\n");
-                        break;
+                        Console.WriteLine("\nOpção inválida!");
+                    break;
                 }
-                Console.WriteLine("Pressione ENTER para continuar...");
-                Console.ReadLine();
-            } while (opcao != '7');
-
-            Console.Write("Obrigado por utilizar o sistema!!");
+            } while (menu != 7 && cadastro < 100);
         }
     }
 }
