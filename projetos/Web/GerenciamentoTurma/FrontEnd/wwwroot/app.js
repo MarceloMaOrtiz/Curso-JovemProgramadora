0915c9ec3cb96300d8d67b6b927b8e00729704dc
marce <marce@PAICINCIA>
2025-10-02 17:13:47 -0300

    atualizações

--- projetos/Web/GerenciamentoTurma/FrontEnd/wwwroot/app.js
+++ projetos/Web/GerenciamentoTurma/FrontEnd/wwwroot/app.js
@@ -1,13 +1,23 @@
-﻿const API = "http://localhost:5000/api/alunos"; // ajuste porta da WebApi
+﻿const API = "https://localhost:7085/api/alunos"; // ajuste porta da WebApi
+
+function mascaraData(dataIso) {
+    const [ano, mes, dia] = dataIso.split("-");
+    return `${dia}/${mes}/${ano}`;
+}
+
+function mascaraCpf(cpf) {
+    cpf = cpf.replace(/\D/g, "");
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
 
 document.getElementById("btnCarregar").addEventListener("click", async () => {
     const resp = await fetch(API);
     const json = await resp.json();
     const ul = document.getElementById("lista");
     ul.innerHTML = "";
-    json.dados.forEach(a => {
+    json.objeto.forEach(aluno => {
         const li = document.createElement("li");
-        li.textContent = a.nome;
+        li.textContent = `ID: ${aluno.id} | Nome: ${aluno.nome} | Nascimento: ${mascaraData(aluno.dataNascimento.valor)} | CPF: ${mascaraCpf(aluno.cpf.valor)} | Média: ${aluno.media}`;
         ul.appendChild(li);
     });
 });
--- projetos/Web/GerenciamentoTurma/WebApi/Program.cs
+++ projetos/Web/GerenciamentoTurma/WebApi/Program.cs
@@ -2,6 +2,17 @@ using WebApi.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurando o CORS, responsável por permitir o acesso do front-end ao back-end
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:7069") // Porta do seu front-end
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,8 +29,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapWeatherForecastEndpoints();
+// Habilita o CORS
+app.UseCors();
 
+app.MapWeatherForecastEndpoints();
 app.MapGerenciamentoTurmaEndpoints();
 
 app.Run();
